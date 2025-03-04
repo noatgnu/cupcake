a183f24faa9f1c31a4037b9477d31d7ab132f47d
Toan Phung <toan.phung@proteo.info>
2025-03-04 12:01:48 +0000

    Update filters.py

--- cc/filters.py
+++ cc/filters.py
@@ -3,7 +3,8 @@ from .models import Unimod
 
 class UnimodFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(field_name='name', lookup_expr='startswith')
+    definition = django_filters.CharFilter(field_name='definition', lookup_expr='icontains')
 
     class Meta:
         model = Unimod
-        fields = ['name']
+        fields = ['name', 'definition']
